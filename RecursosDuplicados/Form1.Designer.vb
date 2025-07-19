df15eaa4468aeae152234255c309519fc52e4a60
César Martínez <40126795+tacvba69@users.noreply.github.com>
2025-07-19 21:59:02 +0200

    Agregar archivos de proyecto.

---
+++ RecursosDuplicados/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        ToolStrip1 = New ToolStrip()
+        ToolStripButton1 = New ToolStripButton()
+        ToolStripSeparator1 = New ToolStripSeparator()
+        btnEliminarSeleccionados = New ToolStripButton()
+        StatusStrip1 = New StatusStrip()
+        ToolStripProgressBar1 = New ToolStripProgressBar()
+        lblProgreso = New ToolStripStatusLabel()
+        lvDuplicados = New ListView()
+        ToolStripButton2 = New ToolStripButton()
+        ToolStripSeparator2 = New ToolStripSeparator()
+        ToolStrip1.SuspendLayout()
+        StatusStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' ToolStrip1
+        ' 
+        ToolStrip1.Items.AddRange(New ToolStripItem() {ToolStripButton1, ToolStripSeparator1, ToolStripButton2, ToolStripSeparator2, btnEliminarSeleccionados})
+        ToolStrip1.Location = New Point(0, 0)
+        ToolStrip1.Name = "ToolStrip1"
+        ToolStrip1.Size = New Size(1000, 25)
+        ToolStrip1.TabIndex = 2
+        ToolStrip1.Text = "ToolStrip1"
+        ' 
+        ' ToolStripButton1
+        ' 
+        ToolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image
+        ToolStripButton1.Image = My.Resources.Resources.folder_explore
+        ToolStripButton1.ImageTransparentColor = Color.Magenta
+        ToolStripButton1.Name = "ToolStripButton1"
+        ToolStripButton1.Size = New Size(23, 22)
+        ToolStripButton1.Text = "Buscar"
+        ' 
+        ' ToolStripSeparator1
+        ' 
+        ToolStripSeparator1.Name = "ToolStripSeparator1"
+        ToolStripSeparator1.Size = New Size(6, 25)
+        ' 
+        ' btnEliminarSeleccionados
+        ' 
+        btnEliminarSeleccionados.DisplayStyle = ToolStripItemDisplayStyle.Image
+        btnEliminarSeleccionados.Image = My.Resources.Resources.delete_x16
+        btnEliminarSeleccionados.ImageTransparentColor = Color.Magenta
+        btnEliminarSeleccionados.Name = "btnEliminarSeleccionados"
+        btnEliminarSeleccionados.Size = New Size(23, 22)
+        btnEliminarSeleccionados.Text = "Eliminar Archivos Seleccionados"
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripProgressBar1, lblProgreso})
+        StatusStrip1.Location = New Point(0, 488)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(1000, 22)
+        StatusStrip1.TabIndex = 4
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripProgressBar1
+        ' 
+        ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        ToolStripProgressBar1.Size = New Size(200, 16)
+        ' 
+        ' lblProgreso
+        ' 
+        lblProgreso.AutoSize = False
+        lblProgreso.Name = "lblProgreso"
+        lblProgreso.Size = New Size(300, 17)
+        lblProgreso.Text = "ToolStripStatusLabel1"
+        lblProgreso.TextAlign = ContentAlignment.MiddleLeft
+        ' 
+        ' lvDuplicados
+        ' 
+        lvDuplicados.CheckBoxes = True
+        lvDuplicados.Dock = DockStyle.Fill
+        lvDuplicados.Location = New Point(0, 25)
+        lvDuplicados.Name = "lvDuplicados"
+        lvDuplicados.Size = New Size(1000, 463)
+        lvDuplicados.TabIndex = 5
+        lvDuplicados.UseCompatibleStateImageBehavior = False
+        ' 
+        ' ToolStripButton2
+        ' 
+        ToolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image
+        ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), Image)
+        ToolStripButton2.ImageTransparentColor = Color.Magenta
+        ToolStripButton2.Name = "ToolStripButton2"
+        ToolStripButton2.Size = New Size(23, 22)
+        ToolStripButton2.Text = "ToolStripButton2"
+        ' 
+        ' ToolStripSeparator2
+        ' 
+        ToolStripSeparator2.Name = "ToolStripSeparator2"
+        ToolStripSeparator2.Size = New Size(6, 25)
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1000, 510)
+        Controls.Add(lvDuplicados)
+        Controls.Add(StatusStrip1)
+        Controls.Add(ToolStrip1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Borrar Archivos Duplicados"
+        WindowState = FormWindowState.Maximized
+        ToolStrip1.ResumeLayout(False)
+        ToolStrip1.PerformLayout()
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents btnEliminarSeleccionados As ToolStripButton
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
+    Friend WithEvents lvDuplicados As ListView
+    Friend WithEvents lblProgreso As ToolStripStatusLabel
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+
+End Class
---
+++ RecursosDuplicados/Form1.vb
@@ -0,0 +1,156 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Linq
+Imports System.Threading.Tasks
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lvDuplicados.View = View.Details
+        lvDuplicados.CheckBoxes = True
+        lvDuplicados.Columns.Add("Archivo", 500)
+        lvDuplicados.Columns.Add("Ruta", 700)
+        lvDuplicados.Columns.Add("Tamaño", 100)
+        lvDuplicados.Columns.Add("Tipo", 100)
+        lvDuplicados.FullRowSelect = True
+        lvDuplicados.GridLines = True
+
+
+        ToolStripProgressBar1.Visible = False
+        lblProgreso.Text = ""
+    End Sub
+
+    Private Async Sub BuscarDuplicados()
+        Using folderDlg As New FolderBrowserDialog()
+            If folderDlg.ShowDialog() = DialogResult.OK Then
+                Dim rutaCarpeta As String = folderDlg.SelectedPath
+                lvDuplicados.Items.Clear()
+
+                ToolStripProgressBar1.Visible = True
+                ToolStripProgressBar1.Value = 0
+                lblProgreso.Text = "Iniciando análisis..."
+
+                Dim duplicados As List(Of List(Of String)) = Await Task.Run(Function() ObtenerDuplicadosConProgreso(rutaCarpeta))
+
+                Dim contadorImg As Integer = 0
+                If duplicados.Count = 0 Then
+                    lblProgreso.Text = "No se encontraron duplicados."
+                    MessageBox.Show("No se encontraron duplicados.")
+                Else
+                    For Each grupo As List(Of String) In duplicados
+                        Dim copiasPermitidas As Integer = grupo.Count - 1
+                        Dim contadorCheck As Integer = 0
+                        For Each archivo As String In grupo
+                            Try
+                                Dim fi As New FileInfo(archivo)
+                                Dim nombre = Path.GetFileName(archivo)
+                                Dim item As New ListViewItem(nombre)
+                                item.SubItems.Add(archivo)
+                                item.SubItems.Add(ConvertirTamanio(fi.Length))
+                                item.SubItems.Add(fi.Extension.ToLower())
+
+                                Try
+                                    Dim ext As String = fi.Extension.ToLower()
+                                    If ext = ".jpg" OrElse ext = ".jpeg" OrElse ext = ".png" OrElse ext = ".bmp" OrElse ext = ".gif" Then
+                                        Dim imgBytes As Byte() = File.ReadAllBytes(archivo)
+                                        Using ms As New MemoryStream(imgBytes)
+                                            Dim img As Image = Image.FromStream(ms)
+                                            item.ImageKey = contadorImg.ToString()
+                                            contadorImg += 1
+                                        End Using
+                                    End If
+                                Catch ex As Exception
+                                    ' Ignorar imágenes no válidas
+                                End Try
+
+                                If contadorCheck < copiasPermitidas Then
+                                    item.Checked = True
+                                    contadorCheck += 1
+                                End If
+
+                                lvDuplicados.Items.Add(item)
+                            Catch ex As Exception
+                                ' Ignorar error
+                            End Try
+                        Next
+                        Dim separador As New ListViewItem("---")
+                        separador.ForeColor = Color.Gray
+                        separador.Checked = False
+                        separador.BackColor = Color.LightGray
+                        separador.SubItems.Add("")
+                        separador.SubItems.Add("")
+                        separador.SubItems.Add("")
+                        lvDuplicados.Items.Add(separador)
+                    Next
+                    lblProgreso.Text = $"Se encontraron {duplicados.Count} grupos de archivos duplicados."
+                End If
+
+                lvDuplicados.View = View.LargeIcon
+                ToolStripProgressBar1.Visible = False
+            End If
+        End Using
+    End Sub
+
+    Private Function ObtenerDuplicadosConProgreso(ruta As String) As List(Of List(Of String))
+        Dim archivos As String() = Directory.GetFiles(ruta, "*.*", SearchOption.AllDirectories)
+        Dim hashDic As New Dictionary(Of String, List(Of String))()
+        Dim total As Integer = archivos.Length
+
+        Invoke(Sub()
+                   ToolStripProgressBar1.Maximum = total
+               End Sub)
+
+        For i As Integer = 0 To archivos.Length - 1
+            Try
+                Using fs As FileStream = File.OpenRead(archivos(i))
+                    Dim md5 As MD5 = MD5.Create()
+                    Dim hash As String = BitConverter.ToString(md5.ComputeHash(fs)).Replace("-", "").ToLower()
+                    If Not hashDic.ContainsKey(hash) Then
+                        hashDic(hash) = New List(Of String)()
+                    End If
+                    hashDic(hash).Add(archivos(i))
+                End Using
+            Catch ex As Exception
+                ' Ignorar errores de lectura
+            End Try
+
+            Dim iCopy = i
+            Dim gruposEncontrados = hashDic.Values.Where(Function(l) l.Count > 1).Count()
+            Invoke(Sub()
+                       ToolStripProgressBar1.Value = iCopy + 1
+                       lblProgreso.Text = $"Revisando archivo {iCopy + 1} de {archivos.Length}... Grupos encontrados: {gruposEncontrados}"
+                   End Sub)
+        Next
+
+        Return hashDic.Values.Where(Function(list) list.Count > 1).ToList()
+    End Function
+
+    Private Function ConvertirTamanio(bytes As Long) As String
+        Dim kb As Double = bytes / 1024.0
+        If kb < 1024 Then
+            Return $"{Math.Round(kb, 2)} KB"
+        Else
+            Dim gb As Double = kb / 1024.0
+            Return $"{Math.Round(gb, 2)} MB"
+        End If
+    End Function
+
+    ' Botón buscar
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        BuscarDuplicados()
+    End Sub
+
+
+    Private Sub lvDuplicados_DoubleClick(sender As Object, e As EventArgs) Handles lvDuplicados.DoubleClick
+        If lvDuplicados.SelectedItems.Count > 0 Then
+            Dim ruta As String = lvDuplicados.SelectedItems(0).SubItems(1).Text
+            If File.Exists(ruta) Then
+                Process.Start("explorer.exe", "/select," & ruta)
+            End If
+        End If
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        lvDuplicados.View = View.Details
+    End Sub
+End Class
